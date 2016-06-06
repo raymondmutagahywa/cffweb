509b5b7b3784ca6b397b38e2501223eff15ea4b4
raymondmutagahywa <raymond.mutagahywa@gmail.com>
2016-06-06 19:13:44 +0200

    Completed menu bar , side navigation and routing

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('wce2')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
